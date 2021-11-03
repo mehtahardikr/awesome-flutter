ecb2dc5ca013010b5e611bc1ff1a70923965c77c
hardik mehta <hardikm@unoindia.co>
2021-11-03 10:41:13 +0530

    listview with divider added

--- example/listeview_demo.dart
+++ example/listeview_demo.dart
@@ -11,24 +11,51 @@ class _SimpleDemoNewState extends State<SimpleDemoNew> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: SafeArea(
-         child: Column(
-           children: [
-             ListView.builder(itemBuilder: (BuildContext context , int i){
-                   return _item(i);
-             } , shrinkWrap: true, itemCount: 5,),
-           ],
-         ),
-       ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            _simpleList(),
+            SizedBox(
+              height: 15.00,
+            ),
+            _simpleListWithDivider(),
+          ],
+        ),
+      ),
     );
   }
 
   ///  list item
-  Widget _item( int index)  {
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text('row :  $index'),
-      );
+  Widget _item(int index) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text('row :  $index'),
+    );
   }
 
+  /// simple list
+  Widget _simpleList() {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int i) {
+        return _item(i);
+      },
+      shrinkWrap: true,
+      itemCount: 5,
+    );
+  }
+
+  ///  list with divider
+  Widget _simpleListWithDivider() {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int i) {
+        return _item(i);
+      },
+      shrinkWrap: true,
+      itemCount: 5,
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider();
+      },
+    );
+  }
 }
+
